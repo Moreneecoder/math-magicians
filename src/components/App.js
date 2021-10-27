12e98cdcd44790f31e42b30edf1fbdc5863cce24
Morenikeji Bello <caeserselassie@gmail.com>
2021-10-27 16:53:33 +0100

    Add events to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculator from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <>
-        <Display result="23" />
-        <ButtonPanel />
-        <Calculator />
-      </>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = (data) => {
+      const calculation = Calculator(this.state, data);
+      this.setState(calculation);
+    };
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <div className="App">
+        <>
+          <Display result={next || total} />
+          <ButtonPanel handleClick={this.handleClick} />
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
