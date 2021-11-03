c5995c23d5524301d389162e1c08c438b7137d4a
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-03 17:44:10 +0100

    Add bootstrap-style containers to router component

---
+++ src/components/NavBar.js
@@ -0,0 +1,7 @@
+const NavBar = () => (
+  <div className="Navbar">
+    <h1 className="logo">Math Magicians</h1>
+  </div>
+);
+
+export default NavBar;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import '../stylesheets/Router.css';
+import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import App from '../components/App';
 import Quote from '../components/Quote';
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/calculator" component={App} />
-      <Route path="/quotes" component={Quote} />
-    </Switch>
+    <div className="Router">
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={App} />
+        <Route path="/quotes" component={Quote} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
