60a4f55da6918aa6b49bef83de6a06195e0e7bd0
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-03 13:31:11 +0100

    Add styling appropriately

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,17 @@ import PropTypes from 'prop-types';
 import '../stylesheets/button.css';
 
 const Button = (props) => {
-  const { name, onClick } = props;
+  const { name, onClick, addColor } = props;
 
-  const getButtonClass = (btnName) => (
-    (btnName === '0') ? 'zeroButton' : 'Button'
-  );
+  const getButtonClass = (btnName) => {
+    const classes = [];
+    classes.push((btnName === '0') ? 'zeroButton' : 'Button');
+    classes.push(addColor ? 'defaultBg' : '');
+    return classes;
+  };
 
   return (
-    <div className={getButtonClass(name)}>
+    <div className={getButtonClass(name).join(' ')}>
       <button type="button" onClick={onClick}>{name}</button>
     </div>
   );
@@ -18,6 +21,7 @@ const Button = (props) => {
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  addColor: PropTypes.func.isRequired,
 };
 
 export default Button;
