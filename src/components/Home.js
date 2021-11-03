8d16a1e20597daa2f754219ff3a65475863ff0f7
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-03 19:47:00 +0100

    Implement homepage styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,31 @@
+import '../stylesheets/home.css';
+import Math from '../images/math.svg';
+
 const Home = () => (
   <div className="Home">
-    <h1>No Place Like Home</h1>
+    <img className="mathImg" src={Math} alt="math" />
+    <h1 className="center-align">Where The Magic Of Math Is Stripped Bare</h1>
+
+    <div className="desc">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+    </div>
+
   </div>
 );
 
