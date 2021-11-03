d3102028714858fe5037aafa21d59a5ad86abfa7
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-03 17:50:36 +0100

    Add clickable links for routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = () => (
   <div className="Navbar">
     <h1 className="logo">Math Magicians</h1>
+
+    <div className="navLinks">
+      <Link to="/">Home</Link>
+      <Link to="/calculator">calculator</Link>
+      <Link to="/quotes">Quotes</Link>
+    </div>
   </div>
 );
 
