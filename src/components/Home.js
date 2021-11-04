1947d8d0edd3a1d21d5107024bee755e5d77ace8
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-04 08:52:23 +0100

    Complete Quote.js styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,16 +6,7 @@ const Home = () => (
     <img className="mathImg" src={Math} alt="math" />
     <h1 className="center-align">Where The Magic Of Math Is Stripped Bare</h1>
 
-    <div className="desc">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
-      </p>
-
+    <div className="desc main-bg-color">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,6 +1,15 @@
+import '../stylesheets/quote.css';
+
 const Quote = () => (
   <div className="Quote">
     <h1>Quote &apos;Em Up</h1>
+    <div className="main main-bg-color">
+      “Knowledge is a garden. If it isn’t cultivated, you can’t harvest it.”
+    </div>
+
+    <div className="author main-bg-color">
+      AFRICAN PROVERB
+    </div>
   </div>
 );
 
