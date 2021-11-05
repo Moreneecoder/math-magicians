2b9ecef29f946f9247d761452f8a8c469287d57c
Morenikeji Bello <caeserselassie@gmail.com>
2021-11-05 20:22:33 +0100

    Remove unnecessary comments from Home.test.js

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,16 +1,5 @@
-// import { render, screen } from "@testing-library/react"
 import renderer from 'react-test-renderer';
 import Home from '../components/Home';
-
-// test(`on render, page should contain h1 of 'Where Magic Of Maths Is Stripped Bare'`, () => {
-//     render(<Home />)
-
-//     // screen.debug();
-//     expect(screen.getByRole('heading',
-//     {name: /Where The Magic Of Math Is Stripped Bare/}))
-//     .toBeInTheDocument()
-
-// })
 
 describe('Home Component', () => {
   test('Renders home component as intended', () => {
